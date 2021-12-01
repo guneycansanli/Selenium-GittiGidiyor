066d83d92520f723293d9d92f3a6d72fb8e13580
guneycansanli <48263266+guneycansanli@users.noreply.github.com>
2021-12-01 12:50:46 +0300

    Update Utils.java

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -4,8 +4,8 @@ public class Utils {
 
     final static String BASE_URL = "https://gittigidiyor.com/";
     final static String CHROME_DRIVER_LOCATION = "chromedriver.exe";
-    final static String USER_EMAIL = "guneycansanli@hotmail.com";
-    final static String PASSWORD = "123abc";
+    final static String USER_EMAIL = "E-mail";
+    final static String PASSWORD = "Passwod";
     final static String SECOND_PAGE_URL = "https://www.gittigidiyor.com/arama/?k=bilgisayar&sf=2";
 
 }
